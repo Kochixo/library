a79ff4b48c2ebca0c417097b585650a5764b462a
Kochixo <kochixo10@gmail.com>
2024-05-11 14:22:44 +0100

    v1.0

---
+++ src/skip.groovy
@@ -0,0 +1,24 @@
+def skiped_email = "jenkins" 
+
+pipeline {
+    agent any
+    environment {
+        GIT_AUTHOR_EMAIL = ""
+    }
+    stages {
+        stage("get commit") {
+            steps {
+                script {
+                    GIT_AUTHOR_EMAIL = sh(script: 'git log --format="%ae" | head -1', returnStdout: true).trim()
+                }
+            }
+        }
+        stage('Teste') {
+            when {
+                expression {
+                    GIT_AUTHOR_EMAIL != skiped_email
+                }
+            }
+        }
+    }
+}
