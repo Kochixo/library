77c13c152f4723d7f3d4aec9d1e5e48c7132a0d4
Kochixo <kochixo10@gmail.com>
2024-05-14 18:51:24 +0100

    teste

--- vars/skipByCommiter.groovy
+++ vars/skipByCommiter.groovy
@@ -1,10 +1,10 @@
-def call(String email) {
-    echo "PASSED INTO LIB"
-    String teste = allCommitterDetails.Trim()
-    return teste == email;
-    }
-
 def allCommitterDetails = bat ( 
  script: 'git log --format="%ae" | head -1',
  returnStdout: true
 )
+
+def call(String email) {
+    echo "PASSED INTO LIB"
+    String teste = allCommitterDetails.Trim()
+    return teste == email;
+}
