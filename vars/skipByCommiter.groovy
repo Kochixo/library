402e293629ca12e63d807f520e13180c64a80ab6
Kochixo <kochixo10@gmail.com>
2024-05-14 22:30:58 +0100

    v1.0.0

--- vars/skipByCommiter.groovy
+++ vars/skipByCommiter.groovy
@@ -1,9 +1,3 @@
-import hudson.model.AbstractProject
-import hudson.tasks.Mailer
-import hudson.model.User
-import jenkins.model.Jenkins
-
-
 def call() {
     def lastCommitterEmail = sh (
         script: "git log --format=\"%ae\" | head -1",
