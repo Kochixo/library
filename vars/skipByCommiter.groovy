c2286220b8d10426c9d814e200e2acff4962bb81
Kochixo <kochixo10@gmail.com>
2024-05-14 19:43:15 +0100

    tetxt

--- vars/skipByCommiter.groovy
+++ vars/skipByCommiter.groovy
@@ -2,6 +2,11 @@ def call(String email) {
     echo "PASSED INTO LIB"
     def allCommitterDetails = "git log --format=\"%ae\" | head -1".execute()
 
+    def pwd = "pwd".execute()
+
+    echo pwd.in.text
+    echo pwd.err.text
+
     echo allCommitterDetails.in.text
     echo allCommitterDetails.err.text
 
