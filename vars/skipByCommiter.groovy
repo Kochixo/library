54d2a8554121bd663f40dccfa994253bf4605c3d
Kochixo <kochixo10@gmail.com>
2024-05-14 22:14:02 +0100

    trqqeweqw

--- vars/skipByCommiter.groovy
+++ vars/skipByCommiter.groovy
@@ -1,20 +1,15 @@
 import hudson.model.AbstractProject
 import hudson.tasks.Mailer
 import hudson.model.User
+import jenkins.model.Jenkins
+
 
 def call() {
     def lastCommitterEmail = sh (
         script: "git log --format=\"%ae\" | head -1",
         returnStdout: true
     )
-    buildCause = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')
-    id = buildCause[0].userId
-    echo "id: ${id}"
-    User u = User.get(id)
-    echo "u: ${u}"
-    umail = u.getProperty(Mailer.UserProperty.class)
-    echo "umail: ${umail}"
-    email = umail.getAddress()
-    echo "email: ${email}"
-    return lastCommitterEmail.trim() != email.trim();
+    def envVars = Jenkins.instance.getGlobalNodeProperties()[0].getEnvVars() 
+    echo envVars['myVar']
+    return lastCommitterEmail.trim() != umail.getAddress().trim();
 } 
