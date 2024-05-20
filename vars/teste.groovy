252d21053314391df3dc125ad1c1a36aca209098
Kochixo <kochixo10@gmail.com>
2024-05-20 19:31:38 +0100

    asdfg

--- vars/teste.groovy
+++ vars/teste.groovy
@@ -1,7 +1,3 @@
 def call() {
-    checkout scm
-    result = sh (script: "git log -1 | grep '\\[release\\]'", returnStatus: true) 
-    println(result)
-    currentBuild.result = 'ABORTED'
     Run.fromExternalizableId(currentBuild.externalizableId).delete()
 }
